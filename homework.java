b5af91ecbc68b0bad57a9f7f40c9f416d50ecb15
Akie-MinBai <153165498+Akie-MinBai@users.noreply.github.com>
2024-03-04 10:31:09 +0800

    林冰钊

--- homework.java
+++ homework.java
@@ -4,5 +4,6 @@ public class homework {
     public static void main(String[] args) {
         System.out.println("测试 v0.0.0");
         System.out.println("测试 v0.0.1");
+        System.out.println("林冰钊 v1.0.0");
     }
 }
