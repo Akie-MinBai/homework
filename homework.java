b7affa148ca707e9aba5fed1b5ad65b0a7440fec
joshhart9 <2996894055@qq.com>
2024-03-04 18:42:22 +0800

    单泽锋

--- homework.java
+++ homework.java
@@ -4,8 +4,8 @@ public class homework {
     public static void main(String[] args) {
         System.out.println("测试 v0.0.0");
         System.out.println("测试 v0.0.1");
-        System.out.println("林冰钊 v1.0.0");
-        System.out.println("陈瑞基 v2.0.1");
-        System.out.println("单泽锋 v3.0.2");
+        System.out.println("林冰�? v1.0.0");
+        System.out.println("陈瑞�? v2.0.1");
+        System.out.println("单泽�? v3.0.3");
     }
 }
