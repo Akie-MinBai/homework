21029ccbdac8c808e58641e0ae401975b6c0a942
Akie-MinBai <153165498+Akie-MinBai@users.noreply.github.com>
2024-03-04 19:00:28 +0800

    Update homework.java

--- homework.java
+++ homework.java
@@ -3,9 +3,8 @@ package test;
 public class homework {
     public static void main(String[] args) {
         System.out.println("测试 v0.0.0");
-        System.out.println("测试 v0.0.1");
-        System.out.println("林冰�? v1.0.0");
-        System.out.println("陈瑞�? v2.0.1");
-        System.out.println("单泽�? v3.0.3");
+        System.out.println("林冰钊 v0.0.1");
+        System.out.println("陈瑞基 v1.0.0");
+        System.out.println("单泽锋 v2.0.1");
     }
 }
