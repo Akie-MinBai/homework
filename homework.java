028247fa4580bb493a7994896385e6f7a261cd95
22ruanjian3-1 <2608011404@qq.com>
2024-03-05 14:11:27 +0800

    crj12

--- homework.java
+++ homework.java
@@ -3,9 +3,16 @@ package test;
 public class homework {
     public static void main(String[] args) {
         System.out.println("测试 v0.0.0");
+<<<<<<< HEAD
         System.out.println("林冰钊 v0.0.1");
         System.out.println("陈瑞基 v1.0.0");
         System.out.println("单泽锋 v2.0.1");
         System.out.println("陈增财 v3.0.2");
+=======
+        System.out.println("测试 v0.0.1");
+        System.out.println("林冰钊 v1.0.0");
+        System.out.println("陈瑞基 v2.0.1");
+        System.out.println("crj v12.0.0");
+>>>>>>> f707b72 (crj12)
     }
 }
